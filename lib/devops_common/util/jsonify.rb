28b9d461e50405f5ba3446d35262ce40a4928851
Chetan Sarva <chetan@pixelcop.net>
2012-02-09 13:03:57 -0500

    ruby19 fix

--- lib/devops_common/util/jsonify.rb
+++ lib/devops_common/util/jsonify.rb
@@ -3,7 +3,7 @@ require 'json'
 
 module Jsonify
     def to_json(options = nil)
-        self.to_json_properties.inject({}) { |h,k| h[k[1,k.length]] = self.instance_eval(k); h }.to_json
+        self.to_json_properties.inject({}) { |h,k| h[k[1,k.length]] = self.instance_eval(k.to_s); h }.to_json
     end
     def to_json_properties
         self.instance_variables
