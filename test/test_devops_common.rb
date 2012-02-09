4f39ef14b9606d76fc978d04a2143bb3967ec2b7
Chetan Sarva <chetan@pixelcop.net>
2012-02-09 11:16:29 -0500

    updated test

--- test/test_devops_common.rb
+++ test/test_devops_common.rb
@@ -5,6 +5,7 @@ class TestDevopsCommon < MiniTest::Unit::TestCase
   def test_autoloading
     assert_equal(JsonRequest, JsonRequest.new(nil, nil).class)
     assert_equal(BundleNotFound, BundleNotFound.new.class)
+    assert_equal(CommandNotFound, CommandNotFound.new.class)
     assert_equal(CommandSpec, CommandSpec.new.class)
   end
 
