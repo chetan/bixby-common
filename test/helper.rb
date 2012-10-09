22761242ab67c1d05a0128e5ca1c831f8f17b937
Chetan Sarva <chetan@pixelcop.net>
2012-10-08 20:49:24 -0400

    use common helpers from test_guard

--- test/helper.rb
+++ test/helper.rb
@@ -7,67 +7,13 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'minitest/unit'
-require 'turn'
-require 'turn/reporter'
-require 'turn/reporters/outline_reporter'
 
-Turn.config.framework = :minitest
-Turn.config.format = :outline
-
-module Turn
-  class OutlineReporter < Reporter
-    def start_test(test)
-      @stdout = StringIO.new
-      @stderr = StringIO.new
-
-      name = naturalized_name(test)
-
-      io.print "    %-57s" % name
-
-      @stdout.rewind
-      @stderr.rewind
-
-      $stdout = @stdout
-      $stderr = @stderr unless $DEBUG
-    end
-  end
-end
-
-begin
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "/test/"
-  end
-rescue Exception => ex
-end
-
+require 'test_guard'
+TestGuard.load_simplecov()
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'bixby_common'
-
-class MiniTest::Unit::TestCase
-
-  # minitest assert_throws doesn't seem to work properly
-  def assert_throws(clazz, msg = nil, &block)
-    begin
-      yield
-    rescue Exception => ex
-      puts "#{ex.class}: #{ex.message}"
-      puts ex.backtrace.join("\n")
-      if clazz.to_s == ex.class.name then
-        if msg.nil?
-          return
-        elsif msg == ex.message then
-          return
-        end
-      end
-    end
-    flunk("Expected #{mu_pp(clazz)} to have been thrown")
-  end
-
-end
 
 Dir.glob(File.dirname(__FILE__) + "/../lib/**/*.rb").each{ |f| require f }
 
