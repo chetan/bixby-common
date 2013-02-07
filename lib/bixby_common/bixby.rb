127d7e118860e2c75c028b67d1d4f8ba06c3ca6e
Chetan Sarva <chetan@pixelcop.net>
2013-02-07 13:37:06 -0500

    moved path helpers from client

--- lib/bixby_common/bixby.rb
+++ lib/bixby_common/bixby.rb
@@ -1,6 +1,23 @@
 
 module Bixby
   class << self
-    attr_accessor :repo_path, :agent, :manager_uri
+    attr_accessor :manager_uri
+
+    def repo_path
+      path("repo")
+    end
+
+    # Path to BIXBY_HOME
+    def root
+      @root ||= ENV["BIXBY_HOME"]
+    end
+    alias_method :home, :root
+
+    # Helper for creating absolute paths inside BIXBY_HOME
+    def path(*args)
+      File.expand_path(File.join(root, *args))
+    end
+
+
   end
 end
