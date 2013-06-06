c5908f4d986ef56f8a815a4cb62495f3962c4022
Chetan Sarva <chetan@pixelcop.net>
2013-06-06 17:21:12 -0400

    working for multi_json issue #123

--- lib/bixby_common/command_spec.rb
+++ lib/bixby_common/command_spec.rb
@@ -131,6 +131,7 @@ class CommandSpec
     end
 
     @digest = { :digest => bundle_sha.hexdigest(), :files => digests }
+    MultiJson.load_adapter(:json_gem).activate!
     File.open(path+"/digest", 'w'){ |f|
       f.write(MultiJson.dump(@digest, :pretty => true, :adapter => :json_gem) + "\n")
     }
