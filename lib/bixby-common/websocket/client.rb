5970259745c643f640623cdd7fdc556b52ffd669
Chetan Sarva <chetan@pixelcop.net>
2013-08-21 15:14:32 -0400

    automatically reconnect to the server

--- lib/bixby-common/websocket/client.rb
+++ lib/bixby-common/websocket/client.rb
@@ -5,6 +5,7 @@ require 'eventmachine'
 module Bixby
   module WebSocket
 
+    # WebSocket Client
     class Client
 
       include Bixby::Log
@@ -13,37 +14,75 @@ module Bixby
 
       def initialize(url)
         @url = url
+        @tries = 0
+        @exiting = false
       end
 
+      # Start the Client thread
+      #
+      # NOTE: This call never returns!
       def start
+
+        Kernel.trap("EXIT") do
+          @exiting = true
+        end
+
         EM.run {
-          @ws = Faye::WebSocket::Client.new(@url)
-          @api = Bixby::WebSocket::API.new(@ws)
-
-          ws.on :open do |e|
-            begin
-              api.open(e)
-            rescue Exception => ex
-              logger.error ex
-            end
-          end
-
-          ws.on :message do |e|
-            begin
-              api.message(e)
-            rescue Exception => ex
-              logger.error ex
-            end
-          end
-
-          ws.on :close do |e|
-            begin
-              api.close(e)
-            rescue Exception => ex
-              logger.error ex
-            end
-          end
+          reconnect()
         }
+      end
+
+      # Connect to the WebSocket endpoint given by @url. Will attempt to keep
+      # the connection open forever, reconnecting as needed.
+      def reconnect
+        @ws = Faye::WebSocket::Client.new(@url)
+        @api = Bixby::WebSocket::API.new(@ws)
+
+        ws.on :open do |e|
+          begin
+            api.open(e)
+            @tries = 0
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+
+        ws.on :message do |e|
+          begin
+            api.message(e)
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+
+        ws.on :close do |e|
+          begin
+            api.close(e)
+            backoff()
+            reconnect()
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+      end
+
+      # Delay reconnection by a slowly increasing interval
+      def backoff
+        return if @exiting # shutting down, don't try to reconnect
+        @tries += 1
+        if @tries == 1 then
+          logger.debug "retrying immediately"
+        elsif @tries == 2 then
+          logger.debug "retrying every 1 sec"
+          sleep 1
+        elsif @tries <= 30 then
+          sleep 1
+        elsif @tries == 31 then
+          logger.debug "retrying every 5 sec"
+          sleep 5
+        else
+          sleep 5
+        end
       end
 
     end
