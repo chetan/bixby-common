09d3cd38caf46c1858f91c60f07ad95ec1e4c62d
Chetan Sarva <chetan@pixelcop.net>
2013-08-29 13:16:30 -0400

    added Message.to_s

--- lib/bixby-common/util/debug.rb
+++ lib/bixby-common/util/debug.rb
@@ -15,5 +15,31 @@ module Bixby
       end
     end # :nocov:
 
+    # Pretty print a hash
+    #
+    # @example
+    #   {
+    #     content-md5   => "Rb0qo2Ae7KGcS5TDulOjYw==",
+    #     date          => "Thu, 29 Aug 2013 13:49:53 GMT",
+    #     authorization => "APIAuth c61ca57b8d7b3e95fba06a",
+    #   }
+    #
+    # @param [Hash] hash
+    # @return [String]
+    def self.pretty_hash(hash)
+      return "{}" if hash.empty?
+
+      s = [ "\n\t{" ]
+      l = hash.keys.max_by{ |k| k.length }.length + 1 # length of longest key so we can align values
+      hash.keys.each{ |k| s << ("  %s%s=> %s," % [k, " "*(l-k.length), hash[k].inspect]) }
+      s << "}"
+
+      return s.join("\n\t")
+    end
+
+    def self.indent_lines(str, indent="\t")
+      str.gsub(/\n/, "\n#{indent}")
+    end
+
   end # Debug
 end # Bixby
--- lib/bixby-common/websocket/message.rb
+++ lib/bixby-common/websocket/message.rb
@@ -42,6 +42,19 @@ module Bixby
         @body
       end
 
+      # Convert object to String, useful for debugging
+      #
+      # @return [String]
+      def to_s # :nocov:
+        s = []
+        s << "#{self.class}:#{self.object_id}"
+        s << "  id:       #{self.id}"
+        s << "  type:     #{self.type}"
+        s << "  headers:  " + Debug.pretty_hash(self.headers)
+        s << "  body:     " + Debug.pretty_hash(MultiJson.load(self.body))
+        s.join("\n")
+      end # :nocov:
+
     end
 
   end
