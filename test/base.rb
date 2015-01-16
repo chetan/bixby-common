f4cf9891cbcf584b9f5479133699a89ebc67c56c
Chetan Sarva <chetan@pixelcop.net>
2015-01-16 16:05:45 -0500

    updated base TestCase to use micron features

--- test/base.rb
+++ test/base.rb
@@ -1,7 +1,13 @@
 
+require 'micron/test_case/redir_logging'
+
 module Bixby
   module Test
-    class TestCase < MiniTest::Unit::TestCase
+    class TestCase < Micron::TestCase
+
+      include Bixby::Log
+      include Micron::TestCase::RedirLogging
+      self.redir_logger = Logging.logger[Bixby]
 
       def setup
         ENV["BIXBY_HOME"] = File.join(File.expand_path(File.dirname(__FILE__)), "support")
